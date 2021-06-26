ccd45c305a4af9e1828495d01b629ca2d082530d
abir-ashik-abir <abirashik11@gmail.com>
2021-06-26 12:00:48 +0600

    .

---
+++ Calculator/app.js
@@ -0,0 +1,27 @@
+const btnOne = document.querySelector('#one');
+const btnTwo = document.querySelector('#two');
+
+btnOne.addEventListener('click', () => {
+    console.log(btnOne.value);
+});
+btnTwo.addEventListener('click', () => {
+    console.log(btnTwo.value);
+});
+
+
+
+
+
+
+
+// const three = document.querySelector('one');
+// const four = document.querySelector('one');
+// const five = document.querySelector('one');
+// const six = document.querySelector('one');
+// const seven = document.querySelector('one');
+// const eight = document.querySelector('one');
+// const nine = document.querySelector('one');
+// const one = document.querySelector('one');
+// const one = document.querySelector('one');
+// const one = document.querySelector('one');
+// const one = document.querySelector('one');
